ca13d5048df2e00fc7f4ec9e82e96607969b0218
misi3k <misi3k@pld-linux.org>
2003-04-06 08:45:00 +0000

    - added comment to DISTCC_HOSTS - moved log file from ~/ to ~/tmp
    Changed files:
        distcc.csh -> 1.2
        distcc.sh -> 1.2

--- distcc.csh
+++ distcc.csh
@@ -1,1 +1,4 @@
+# $Revision$, $Date$
+# DISTCC_HOSTS HOSTNAME[:PORT][,OPT...]
 setenv DISTCC_HOSTS "localhost"
+setenv DISTCC_LOG "$HOME/tmp/distcc.log"
--- distcc.sh
+++ distcc.sh
@@ -1,1 +1,5 @@
-export DISTCC_HOSTS="localhost"
+# $Revision$, $Date$
+# DISTCC_HOSTS HOSTNAME[:PORT][,OPT...]
+DISTCC_HOSTS="localhost"
+DISTCC_LOG="$HOME/tmp/distcc.log"
+export DISTCC_HOSTS DISTCC_LOG
