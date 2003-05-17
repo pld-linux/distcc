3c4996b58595a3e41b805294e4639126cc78e916
cvs2git <feedback@pld-linux.org>
2003-05-17 19:40:15 +0000

    This commit was manufactured by cvs2git to create branch 'unlabeled-1.22.2'.
    Sprout from master 2003-05-17 19:40:15 UTC misi3k <misi3k@pld-linux.org> '- updated to 2.3'
    Delete:
        distcc-hostfile.patch
        distcc-hostopt.patch
        distcc-user.patch
        distcc.config
        distcc.csh
        distcc.inetd
        distcc.init
        distcc.sh
        distccd-version.patch

--- distcc.csh
+++
@@ -1,4 +0,0 @@
-# $Revision$, $Date$
-# DISTCC_HOSTS HOSTNAME[:PORT][,OPT...]
-setenv DISTCC_HOSTS "localhost"
-setenv DISTCC_LOG "$HOME/tmp/distcc.log"
--- distcc.sh
+++
@@ -1,5 +0,0 @@
-# $Revision$, $Date$
-# DISTCC_HOSTS HOSTNAME[:PORT][,OPT...]
-DISTCC_HOSTS="localhost"
-DISTCC_LOG="$HOME/tmp/distcc.log"
-export DISTCC_HOSTS DISTCC_LOG
