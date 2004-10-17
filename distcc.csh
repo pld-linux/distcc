b815ae67868d2836624238ccf11c24a1c1c4efc0
Arkadiusz Miśkiewicz <arekm@maven.pl>
2004-10-17 16:23:29 +0000

    - DON'T override local configuration, arggggggggghhhhhhhhh
    Changed files:
        distcc.csh -> 1.3
        distcc.sh -> 1.3

--- distcc.csh
+++ distcc.csh
@@ -1,4 +1,2 @@
 # $Revision$, $Date$
-# DISTCC_HOSTS HOSTNAME[:PORT][,OPT...]
-setenv DISTCC_HOSTS "localhost"
 setenv DISTCC_LOG "$HOME/tmp/distcc.log"
--- distcc.sh
+++ distcc.sh
@@ -1,5 +1,3 @@
 # $Revision$, $Date$
-# DISTCC_HOSTS HOSTNAME[:PORT][,OPT...]
-DISTCC_HOSTS="localhost"
 DISTCC_LOG="$HOME/tmp/distcc.log"
-export DISTCC_HOSTS DISTCC_LOG
+export DISTCC_LOG
